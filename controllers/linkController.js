dcb066240a816ad35fe96e9086beb62c1f21af45
Igor2kLevi <99228640+Igor2kLevi@users.noreply.github.com>
2024-06-10 21:45:13 -0300

    page styling and adding edit

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -3,8 +3,7 @@ const Link = require('../models/Link')
 const redirect = async (req, res, next) => {
     let title = req.params.title
     try {
-      let doc = await Link.findOne({ title })
-      console.log(doc)
+      let doc = await Link.findOneAndUpdate({ title }, {$inc: { click: 1 } })
       if (doc) {
         res.redirect(doc.url)
       } else {
@@ -29,8 +28,8 @@ const addLink = async (req, res) => {
 
 const allLinks = async (req, res) => {
   try {
-    let links = await Link.find({})
-    res.render('all', { links })
+    let docs = await Link.find({})
+    res.render('all', { links: docs })
   } catch (error) {
     res.send(error)
   }
@@ -52,4 +51,37 @@ const deleteLinks = async (req, res) => {
   }
 }
 
-module.exports = { redirect, addLink, allLinks, deleteLinks }
+const loadLink = async (req, res) => {
+  let id = req.params.id
+
+  try {
+    let doc = await Link.findById(id)
+    res.render('edit', { error: false, body: doc })
+  } catch (error) {
+    res.status(404).send(error)
+  }
+}
+
+const editLink = async (req, res) => {
+
+  let link = {}
+
+  link.title = req.body.title
+  link.description = req.body.description
+  link.url = req.body.url
+
+  let id = req.params.id
+
+  if(!id) {
+    id = req.body.id
+  }
+
+  try{
+    let doc = await Link.updateOne({_id: id}, link)
+    res.redirect('/')
+  } catch (error){
+    res.render('edit', { error, body: req.body })
+  }
+}
+
+module.exports = { redirect, addLink, allLinks, deleteLinks, loadLink, editLink }
--- routes/linkRoute.js
+++ routes/linkRoute.js
@@ -7,9 +7,13 @@ router.get('/', linkController.allLinks)
 
 router.get('/add', (req, res) => res.render('index', { error: false, body:{} }))
 
-router.get('/:title', linkController.redirect)  
+router.get('/:title', linkController.redirect)
+
+router.get('/edit/:id', linkController.loadLink)
 
 router.post('/', express.urlencoded({ extended: true }), linkController.addLink)
+
+router.post('/edit/:id', express.urlencoded({ extended: true }), linkController.editLink )
 
 router.delete('/:id', linkController.deleteLinks)
 
